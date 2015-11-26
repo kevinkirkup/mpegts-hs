dc3144a4194b8159f79ac4d36754ac7b5b88f60e
Kevin S Kirkup <kevin.s.kirkup@me.com>
2015-11-26 11:50:51 -0500

    Add Initial command line option parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
 import Lib
+import System.Console.ArgParser
+import Control.Applicative
+
+{-
+Data type to hold the command line optio
+-}
+data MyConfig = MyConfig String Bool
+  deriving Show
+
+parser :: ParserSpec MyConfig
+parser = MyConfig
+  `parsedBy` reqPos "input" `Descr` "Input MpegTs file"
+  `andBy` boolFlag "verbose" `Descr` "Verbose output"
+
+commandLineInterface :: IO (CmdLnInterface MyConfig)
+commandLineInterface = 
+  (`setAppDescr` "Parser for printing out information about an MpegTS file")
+  <$> (`setAppEpilog` "Kevin S Kirkup")
+  <$> mkApp parser
+
 
 main :: IO ()
-main = someFunc
+main = do
+  interface <- commandLineInterface
+  runApp interface print
+  someFunc
+
